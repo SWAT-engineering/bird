1fc761a8c9be2c165c72fedd0deec2ca9ea6b562
Pablo Inostroza Valdera <pvaldera@cwi.nl>
2019-03-29 15:13:43 +0100

    Adapting `Value` to new interface in Metal

--- bird-core/java-src/engineering/swat/algorithms/CRC.java
+++ bird-core/java-src/engineering/swat/algorithms/CRC.java
@@ -7,6 +7,7 @@ import java.util.zip.CRC32;
 
 import engineering.swat.function.UnaryFunction;
 import io.parsingdata.metal.encoding.Encoding;
+import io.parsingdata.metal.expression.value.CoreValue;
 import io.parsingdata.metal.expression.value.Value;
 
 public class CRC extends UnaryFunction{
@@ -14,9 +15,9 @@ public class CRC extends UnaryFunction{
 	@Override
 	public Optional<Value> internalApply(Value value, Encoding encoding) {
 		final CRC32 crc = new CRC32();
-        crc.update(value.getValue());
+        crc.update(value.value());
         final long crcValue = crc.getValue();
-        return Optional.of(new Value(createFromBytes(encoding.byteOrder.apply(new byte[] {
+        return Optional.of(new CoreValue(createFromBytes(encoding.byteOrder.apply(new byte[] {
             (byte)((crcValue & 0xff000000) >> 24),
             (byte)((crcValue & 0xff0000) >> 16),
             (byte)((crcValue & 0xff00) >> 8),
