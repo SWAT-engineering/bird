b266e367f3d29f72a163b9a280ee391c93b796e4
Davy Landman <davy.landman@swat.engineering>
2019-04-27 23:22:55 +0200

    Rewrote PNG description based on the new style

--- nest/test/engineering/swat/nest/examples/png/PNG.java
+++ nest/test/engineering/swat/nest/examples/png/PNG.java
@@ -2,39 +2,157 @@ package engineering.swat.nest.examples.png;
 
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
+import engineering.swat.nest.core.ParseError;
 import engineering.swat.nest.core.bytes.ByteStream;
 import engineering.swat.nest.core.bytes.Context;
 import engineering.swat.nest.core.bytes.TrackedBytesView;
-import engineering.swat.nest.core.tokens.Token;
+import engineering.swat.nest.core.nontokens.NestString;
+import engineering.swat.nest.core.nontokens.NonTokenBytes;
 import engineering.swat.nest.core.tokens.TokenList;
+import engineering.swat.nest.core.tokens.UnsignedBytes;
 import engineering.swat.nest.core.tokens.UserDefinedToken;
 
-public class PNG extends UserDefinedToken {
-	public Signature $dummy1;
-	public TokenList<Chunk> chunks;
-	public IEND $dummy2;
+public class PNG {
+    public final static class PNG$ extends UserDefinedToken {
+        public Signature $dummy1;
+        public TokenList<Chunk> chunks;
+        public IEND $dummy2;
 
-	private PNG() {}
-	
-	public static PNG parse(ByteStream source, Context ctx) {
-		ctx = ctx.setEncoding(StandardCharsets.US_ASCII);
-		ctx = ctx.setByteOrder(ByteOrder.BIG_ENDIAN);
-		PNG result = new PNG();
-		result.$dummy1 = Signature.parse(source, ctx);
-		result.chunks = TokenList.untilParseFailure(source, ctx, (s, c) -> Chunk.parse(s, c));
-		result.$dummy2 = IEND.parse(source, ctx);
-		return result;
-	}
+        private PNG$() {}
+        
+        public static PNG$ parse(ByteStream source, Context ctx) {
+            ctx = ctx.setEncoding(StandardCharsets.US_ASCII);
+            ctx = ctx.setByteOrder(ByteOrder.BIG_ENDIAN);
+            PNG$ result = new PNG$();
+            result.$dummy1 = Signature.parse(source, ctx);
+            result.chunks = TokenList.untilParseFailure(source, ctx, (s, c) -> Chunk.parse(s, c));
+            result.$dummy2 = IEND.parse(source, ctx);
+            return result;
+        }
 
-	@Override
-	public TrackedBytesView getTrackedBytes() {
-		return buildTrackedView($dummy1, chunks, $dummy2);
-	}
+        @Override
+        public TrackedBytesView getTrackedBytes() {
+            return buildTrackedView($dummy1, chunks, $dummy2);
+        }
 
-	@Override
-	public long size() {
-		return $dummy1.size() + chunks.size() + $dummy2.size();
-	}
+        @Override
+        public long size() {
+            return $dummy1.size() + chunks.size() + $dummy2.size();
+        }
+    }
+
+    public static final class Signature extends UserDefinedToken {
+        public UnsignedBytes $dummy1;
+        public UnsignedBytes $dummy2;
+        public UnsignedBytes $dummy3;
+        
+        private Signature() {}
+        
+        public static Signature parse(ByteStream source, Context ctx) throws ParseError {
+            Signature result = new Signature();
+
+            result.$dummy1 = source.readUnsigned(1, ctx);
+            if (!(result.$dummy1.asInteger().getValue() == 0x89)) {
+                throw new ParseError("Signature.$dummy1", result.$dummy1);
+            }
+
+            result.$dummy2 = source.readUnsigned(3, ctx);
+            if (!(result.$dummy2.asString().equals(new NestString("PNG")))) {
+                throw new ParseError("Signature.$dummy2", result.$dummy2);
+            }
+
+            result.$dummy3 = source.readUnsigned(4, ctx);
+            if (!(result.$dummy3.sameBytes(NonTokenBytes.of(0x0d, 0x0a, 0x1a, 0x0a)))) {
+                throw new ParseError("Signature.$dummy3", result.$dummy3);
+            }
+            return result;
+        }
+        
+        @Override
+        public TrackedBytesView getTrackedBytes() {
+            return buildTrackedView($dummy1, $dummy2, $dummy3);
+        }
+        
+        @Override
+        public long size() {
+            return $dummy1.size() + $dummy2.size() + $dummy3.size();
+        }
+        
+
+    }
+
+    public final static class Chunk extends UserDefinedToken {
+        public UnsignedBytes length;
+        public UnsignedBytes type;
+        public UnsignedBytes data;
+        public UnsignedBytes crc;
+        
+        private Chunk() {}
+
+        public static Chunk parse(ByteStream source, Context ctx)  {
+            Chunk result = new Chunk();
+            result.length = source.readUnsigned(4, ctx);
+            result.type = source.readUnsigned(4, ctx);
+            if (!(!result.type.asString().equals("IEND"))) {
+                throw new ParseError("Chunk.type");
+            }
+            result.data = source.readUnsigned(Math.toIntExact(result.length.asInteger().getValue()), ctx);
+            result.crc = source.readUnsigned(4, ctx);
+            if (!(UserDefinedPNG.crc32(TokenList.of(result.type, result.data)) == result.crc.asInteger().getValue())) {
+                throw new ParseError("Chunk.crc");
+            }
+            return result;
+        }
+
+        @Override
+        public TrackedBytesView getTrackedBytes() {
+            return buildTrackedView(length, type, data, crc);
+        }
+        
+        @Override
+        public long size() {
+            return length.size() + type.size() + data.size() + crc.size();
+        }
+
+    }
+
+    public static final class IEND extends UserDefinedToken {
+        public UnsignedBytes length;
+        public UnsignedBytes type;
+        public UnsignedBytes crc;
+        
+        private IEND() {}
+
+        public static IEND parse(ByteStream source, Context ctx) {
+            IEND result = new IEND();
+            result.length = source.readUnsigned(4, ctx);
+            if (!(result.length.asInteger().getValue() == 0)) {
+                throw new ParseError("IED.length", result.length);
+            }
+            
+            result.type = source.readUnsigned(4, ctx);
+            if (!result.type.asString().equals(new NestString("IEND"))) {
+                throw new ParseError("IED.type", result.type);
+            }
+            
+            result.crc = source.readUnsigned(4, ctx);
+            if (!result.crc.sameBytes(NonTokenBytes.of(0xae, 0x42, 0x60, 0x82))) {
+                throw new ParseError("IED.crc", result.crc);
+            }
+            return result;
+        }
+
+        @Override
+        public TrackedBytesView getTrackedBytes() {
+            return buildTrackedView(length, type, crc);
+        }
+
+        @Override
+        public long size() {
+            return length.size() + type.size() + crc.size();
+        }
+
+    }
+
 }
-
 
--- nest/test/engineering/swat/nest/examples/png/PNGTest.java
+++ nest/test/engineering/swat/nest/examples/png/PNGTest.java
@@ -1,26 +1,40 @@
 package engineering.swat.nest.examples.png;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.util.Objects;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import engineering.swat.nest.CommonTestHelper;
 import engineering.swat.nest.core.bytes.ByteStream;
 import engineering.swat.nest.core.bytes.Context;
 import engineering.swat.nest.core.bytes.source.ByteWindowBuilder;
 
-public class PNGTest {
+public class PNGTest  {
 	
 	@Test
-	void canParsePNG() throws IOException, URISyntaxException {
-		assertNotNull(PNG.parse(buildByteSource("/test.png"), Context.DEFAULT_CONTEXT));
+	void helpEclipse() {}
+	
+	@ParameterizedTest
+	@MethodSource("pngProvider")
+	void pngFilesSucceed(Path pngFile) throws IOException, URISyntaxException {
+		ByteStream stream = new ByteStream(ByteWindowBuilder.convert(Files.newInputStream(pngFile), pngFile.toUri()));
+		assertNotNull(PNG.PNG$.parse(stream, Context.DEFAULT_CONTEXT));
+		assertFalse(stream.hasBytesRemaining(), "Did not consume the whole file: " + stream.getOffset() + " of " + Files.size(pngFile));
 	}
-
-	private ByteStream buildByteSource(String filePath) throws IOException, URISyntaxException {
-		try (InputStream stream = Objects.requireNonNull(PNGTest.class.getResourceAsStream(filePath))) {
-			return new ByteStream(ByteWindowBuilder.convert(stream, PNGTest.class.getResource(filePath).toURI()));
+	
+	private static Stream<Path> pngProvider() {
+		try {
+			return CommonTestHelper.findResources(".png");
+		} catch (IOException | URISyntaxException e) {
+			throw new RuntimeException(e);
 		}
 	}
+	
 
 }
