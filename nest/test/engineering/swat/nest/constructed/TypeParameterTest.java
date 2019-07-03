f22f4e31ecbc4fe6aab4451b2b1ae1e7a7f5a215
Pablo Inostroza Valdera <pvaldera@cwi.nl>
2019-07-03 12:57:34 +0200

    More testing of type parameters

--- nest/test/engineering/swat/nest/constructed/TypeParameterTest.java
+++ nest/test/engineering/swat/nest/constructed/TypeParameterTest.java
@@ -14,35 +14,107 @@ import org.junit.jupiter.api.Test;
 
 public class TypeParameterTest {
 
-    @Test
-    void twoByteDoubleParseWithT() {
-        GenT<UnsignedBytes> parsed = GenT.parse(wrap(1, 2), Context.DEFAULT, (s,c) -> s.readUnsigned(1, c));
-        assertEquals(1, parsed.field1.getByteAt(NestBigInteger.ZERO));
-        assertEquals(2, parsed.field2.getByteAt(NestBigInteger.ZERO));
-    }
+	@Test
+	void twoByteDoubleParseWithT() {
+		GenT<UnsignedBytes> parsed = GenT.parse(wrap(1, 2), Context.DEFAULT, (s, c) -> s.readUnsigned(1, c));
+		assertEquals(1, parsed.field1.getByteAt(NestBigInteger.ZERO));
+		assertEquals(2, parsed.field2.getByteAt(NestBigInteger.ZERO));
+	}
+	
+	@Test
+	void twoByteDoubleParseWithMain() {
+		Main main = Main.parse(wrap(1, 2), Context.DEFAULT);
+		assertEquals(1, main.parsed.field1.getByteAt(NestBigInteger.ZERO));
+		assertEquals(2, main.parsed.field2.getByteAt(NestBigInteger.ZERO));
+	}
+	
+	@Test
+	void twoByteDoubleParseWithMain2() {
+		Main2 main = Main2.parse(wrap(1, 2), Context.DEFAULT);
+		assertEquals(1, main.parsed.field1.a.getByteAt(NestBigInteger.ZERO));
+	}
 
+	private static class Main extends UserDefinedToken {
 
-    private static class GenT<T extends Token> extends UserDefinedToken {
+		public final GenT<UnsignedBytes> parsed;
 
-        public final T field1;
-        public final T field2;
+		private Main(GenT<UnsignedBytes> parsed) {
+			this.parsed = parsed;
+		}
 
-        private GenT(T field1, T field2) {
-            this.field1 = field1;
-            this.field2 = field2;
-        }
+		public static Main parse(ByteStream source, Context ctx) {
+			GenT<UnsignedBytes> parsed = GenT.parse(source, ctx, (s, c) -> s.readUnsigned(1, c));
+			return new Main(parsed);
+		}
 
-        public static <T extends Token> GenT<T> parse(ByteStream source, Context ctx,
-                BiFunction<ByteStream, Context, T> tParser) {
-            T field1 = tParser.apply(source, ctx);
-            T field2 = tParser.apply(source, ctx);
-            return new GenT<>(field1, field2);
-        }
+		@Override
+		protected Token[] parsedTokens() {
+			return new Token[] { parsed };
+		}
 
-        @Override
-        protected Token[] parsedTokens() {
-            return new Token[]{field1, field2};
-        }
-    }
+	}
+	
+	private static class A extends UserDefinedToken {
+
+		public final UnsignedBytes a;
+
+		private A(UnsignedBytes a) {
+			this.a = a;
+		}
+
+		public static A parse(ByteStream source, Context ctx) {
+			UnsignedBytes a = source.readUnsigned(1, ctx);
+			return new A(a);
+		}
+
+		@Override
+		protected Token[] parsedTokens() {
+			return new Token[] { a };
+		}
+
+	}
+
+	private static class Main2 extends UserDefinedToken {
+
+		public final GenT<A> parsed;
+
+		private Main2(GenT<A> parsed) {
+			this.parsed = parsed;
+		}
+
+		public static Main2 parse(ByteStream source, Context ctx) {
+			GenT<A> parsed = GenT.parse(source, ctx, (s, c) -> A.parse(s, c));
+			return new Main2(parsed);
+		}
+
+		@Override
+		protected Token[] parsedTokens() {
+			return new Token[] { parsed };
+		}
+
+	}
+	
+	private static class GenT<T extends Token> extends UserDefinedToken {
+
+		public final T field1;
+		public final T field2;
+
+		private GenT(T field1, T field2) {
+			this.field1 = field1;
+			this.field2 = field2;
+		}
+
+		public static <T extends Token> GenT<T> parse(ByteStream source, Context ctx,
+				BiFunction<ByteStream, Context, T> tParser) {
+			T field1 = tParser.apply(source, ctx);
+			T field2 = tParser.apply(source, ctx);
+			return new GenT<>(field1, field2);
+		}
+
+		@Override
+		protected Token[] parsedTokens() {
+			return new Token[] { field1, field2 };
+		}
+	}
 
 }
