f8bdf06d1c8fa1ade72f388cfe7ac4c64184c4c3
Davy Landman <davy.landman@swat.engineering>
2024-02-08 16:26:50 +0100

    Simplified main runner a bit

--- bird-example/src/main/java/images/PNGParser.java
+++ bird-example/src/main/java/images/PNGParser.java
@@ -10,10 +10,15 @@ import java.net.*;
 public class PNGParser {
     
     public static void main(String[] args) throws IOException, URISyntaxException {
+        var sl = openTestPNG();
+        var p = PNG$.__$PNG.parse(sl, Context.DEFAULT);
+        System.out.println(p);
+        System.out.println("Done parsing, all bytes consumed: " + !sl.hasBytesRemaining());
+    }
+
+    private static ByteStream openTestPNG() throws IOException, URISyntaxException {
         try (var f = new FileInputStream("resources/test/truecolor.png")) {
-            var sl = new ByteStream(ByteSliceBuilder.convert(f, new URI("unknown:///test.png")));
-            System.out.println(PNG$.__$PNG.parse(sl, Context.DEFAULT));
-            System.out.println("Done parsing, any bytes left: " + sl.hasBytesRemaining());
+            return new ByteStream(ByteSliceBuilder.convert(f, new URI("unknown:///test.png")));
         }
     }
 }
