1a58cbbaeb0b2f6818ced6cf31ef4a4ac9e88429
Rodin Aarssen <rodin.aarssen@swat.engineering>
2024-10-09 17:10:46 +0200

    Added wrapper specification for JPEG and PNG, and changed the runner to use this

--- bird-example/src/main/java/Runner.java
+++ bird-example/src/main/java/Runner.java
@@ -2,8 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import engineering.swat.bird.generated.images.JPEG$.__$Format;
-import engineering.swat.bird.generated.images.PNG$.__$PNG;
+import engineering.swat.bird.generated.images.Image$.__$Image;
 import engineering.swat.nest.core.ParseError;
 import engineering.swat.nest.core.bytes.ByteStream;
 import engineering.swat.nest.core.bytes.Context;
@@ -21,32 +20,16 @@ import engineering.swat.nest.core.tokens.primitive.UnsignedBytes;
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.println();
-
-        final String fileName = "resources\\test\\truecolor.png";
-        FileInputStream f = null;
-        ByteStream bs = null;
-        try {
-            f = new FileInputStream(fileName);
-            bs = new ByteStream(ByteSliceBuilder.convert(f, new File(fileName).toURI()));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        __$PNG png = parsePNG(fileName, bs);
-        __$Format jpeg = parseJPEG(fileName, bs);
+        final var fileName = "resources\\test\\truecolor.png";
+        System.out.println("Parsing file " + fileName);
         
-        UserDefinedToken parseTree = null;
-        if (png != null) {
-            parseTree = png;
-        } else if (jpeg != null) {
-            parseTree = jpeg;
-        }
-
-        if (parseTree != null) {
-            System.out.println();
-            var numBytes = parseTree.accept(new BottomUpTokenVisitor<>(
+        try {
+            var f = new FileInputStream(fileName);
+            var bs = new ByteStream(ByteSliceBuilder.convert(f, new File(fileName).toURI()));
+            
+            var image = __$Image.parse(bs, Context.DEFAULT);
+    
+            var numBytes = image.accept(new BottomUpTokenVisitor<>(
                     new TokenVisitor<NestBigInteger>() {
                         @Override
                         public NestBigInteger visitUserDefinedToken(UserDefinedToken value) {
@@ -56,53 +39,34 @@ public class Runner {
                             }
                             return NestBigInteger.ZERO;
                         }
-
+    
                         @Override
                         public NestBigInteger visitOptionalToken(OptionalToken<? extends Token> value) {
                             return NestBigInteger.ZERO;
                         }
-
+    
                         @Override
                         public NestBigInteger visitTerminatedToken(
                                 TerminatedToken<? extends Token, ? extends Token> value) {
                             return NestBigInteger.ZERO;
                         }
-
+    
                         @Override
                         public NestBigInteger visitTokenList(TokenList<? extends Token> value) {
                             return NestBigInteger.ZERO;
                         }
-
+    
                         @Override
                         public NestBigInteger visitUnsignedBytes(UnsignedBytes value) {
                             return value.size();
                         }
                     }, NestBigInteger::add));
             System.out.println("----------------------------------");
-            System.out.println("Parse tree has size " + parseTree.size());
+            System.out.println("Parse tree has size " + image.size());
             System.out.println("Visited " + numBytes + " bytes");
-        }
-    }
-
-    private static __$PNG parsePNG(String fileName, ByteStream bs) {
-        try {
-            __$PNG parseTree = __$PNG.parse(bs, Context.DEFAULT);
-            System.out.println("Successful parse as PNG");
-            return parseTree;
-        } catch (ParseError e) {
-            System.out.println("Could not parse as PNG");
-            return null;
-        }
-    }
-
-    private static __$Format parseJPEG(String fileName, ByteStream bs) {
-        try {
-            __$Format parseTree = __$Format.parse(bs, Context.DEFAULT);
-            System.out.println("Successful parse as JPEG");
-            return parseTree;
-        } catch (ParseError e) {
-            System.out.println("Could not parse as JPEG");
-            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
     }
 }
